1b0ead3bb1cb1676be5d4cacfef6d976ac7fc11a
zhiwuxie <zhiwuxie@users.noreply.github.com>
2015-04-23 00:34:27 -0400

    Update install_uws_test.sh
    need to sudo for yum update

--- install_uws_test.sh
+++ install_uws_test.sh
@@ -6,7 +6,7 @@
 # On a freshly provisioned ec2 instance based on RightImage_CentOS_6.5_x64_v14.1.3.1
 # 
 
-yum -y update
+sudo yum -y update
 wget ftp://fr2.rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/httperf-0.9.0-1.el6.rf.x86_64.rpm
 sudo yum --nogpgcheck -y install ./httperf-0.9.0-1.el6.rf.x86_64.rpm
 wget https://atyu30.googlecode.com/files/autobench-2.1.2-1.el6.x86_64.rpm
