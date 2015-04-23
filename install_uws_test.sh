a71e9261a570ca81696e09571275fa1bedf6a231
zhiwuxie <zhiwuxie@users.noreply.github.com>
2015-04-23 17:58:16 -0400

    Update install_uws_test.sh
    build httperf from source to get around FD_SETSIZE limit

--- install_uws_test.sh
+++ install_uws_test.sh
@@ -6,8 +6,18 @@
 # On a freshly provisioned ec2 instance based on RightImage_CentOS_6.5_x64_v14.1.3.1
 # 
 
-sudo yum -y update
-wget ftp://fr2.rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/httperf-0.9.0-1.el6.rf.x86_64.rpm
-sudo yum --nogpgcheck -y install ./httperf-0.9.0-1.el6.rf.x86_64.rpm
+yum -y update
+yum -y groupinstall "Development Tools"
+sed -i 's/1024/65535/' /usr/include/bits/typesizes.h
+wget http://pkgs.fedoraproject.org/repo/extras/httperf/httperf-0.9.0.tar.gz/2968c36b9ecf3d98fc1f2c1c9c0d9341/httperf-0.9.0.tar.gz
+tar xzvf httperf-0.9.0.tar.gz
+mkdir build
+cd build
+../httperf-0.9.0/configure --enable-debug
+make
+make install
+ln -s /usr/local/bin/httperf /bin/httperf
+#wget ftp://fr2.rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/httperf-0.9.0-1.el6.rf.x86_64.rpm
+#sudo yum --nogpgcheck -y install ./httperf-0.9.0-1.el6.rf.x86_64.rpm
 wget https://atyu30.googlecode.com/files/autobench-2.1.2-1.el6.x86_64.rpm
-sudo yum --nogpgcheck -y install ./autobench-2.1.2-1.el6.x86_64.rpm
+yum --nogpgcheck -y install ./autobench-2.1.2-1.el6.x86_64.rpm
